6cec15a4b91de81c85ff78e7070877e6abfc0768
Winnery05 <elmedhassan2002@gmail.com>
2025-01-18 17:08:57 +0100

    set up server / client sides

---
+++ server/src/main/java/baa3/Baa3Application.java
@@ -0,0 +1,13 @@
+package baa3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baa3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baa3Application.class, args);
+	}
+
+}
